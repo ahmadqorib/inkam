2e69a77145f5c8914d6c470066a179917ba47131
tsm <irfangi564@gmail.com>
2018-12-19 00:50:10 +0700

    update

--- app/main.js
+++ app/main.js
@@ -18,7 +18,7 @@ fetch('/inkam/data/kampus.json')
   return response.json();
 }).then(function(data) {
 	for (var i = 0; i < data.data.length; i++) {
-			console.log(data.data[i].kampus_name.repleace(' ','_'));
+			console.log(data.data[i].kampus_name.replace(' ','_'));
 			
     	var list = '<a class="nav-link" href=kampus/"'+data.data[i].kampus_name.replace(' ','_')+'.html"><img src="/inkam/asset/'+data.data[i].kampus_logo+'"> <span>'+data.data[i].kampus_name+'</span></a>';
 		kampus.insertAdjacentHTML('beforeend', list);
