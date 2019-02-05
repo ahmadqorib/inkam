aaee1efbae6765d221ec797b504c2c8690153e77
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 18:17:45 +0700

    indexdb

--- app/db.js
+++ app/db.js
@@ -1,9 +1,30 @@
-
-var info = indexedDB.open('db_inkam', 1, db => {
-    db.createObjectStore('info');
-  });
+(function() {
+    'use strict';
   
-info.then(function(db) {
+    //check for support
+    if (!('indexedDB' in window)) {
+      console.log('This browser doesn\'t support IndexedDB');
+      return;
+    }
+  
+    var dbPromise = idb.open('test-db4', 1, function(upgradeDb) {
+      if (!upgradeDb.objectStoreNames.contains('people')) {
+        var peopleOS = upgradeDb.createObjectStore('people', {keyPath: 'email'});
+        peopleOS.createIndex('gender', 'gender', {unique: false});
+        peopleOS.createIndex('ssn', 'ssn', {unique: true});
+      }
+      if (!upgradeDb.objectStoreNames.contains('notes')) {
+        var notesOS = upgradeDb.createObjectStore('notes', {autoIncrement: true});
+        notesOS.createIndex('title', 'title', {unique: false});
+      }
+      if (!upgradeDb.objectStoreNames.contains('logs')) {
+        var logsOS = upgradeDb.createObjectStore('logs', {keyPath: 'id',
+          autoIncrement: true});
+      }
+    });
+  })();
+
+  dbPromise.then(function(db) {
     var tx = db.transaction('store', 'readwrite');
     var store = tx.objectStore('store');
     var item = {
