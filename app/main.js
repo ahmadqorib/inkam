252ddc0b69e3b1bc68726f50fde28f918fed657b
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 16:11:57 +0700

    aa

--- app/main.js
+++ app/main.js
@@ -27,11 +27,6 @@ fetch('/inkam/data/kampus.json')
 });
 
 
-  if (!('indexedDB' in window)) {return null;}
-  return idb.open('dashboardr', 1, function(upgradeDb) {
-    if (!upgradeDb.objectStoreNames.contains('events')) {
-      const eventsOS = upgradeDb.createObjectStore('events', {keyPath: 'id'});
-    }
-  });
+
 
 
