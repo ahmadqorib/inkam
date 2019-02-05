ec570c6f8c194db91a73cc50a51c7dea8510d3f6
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 21:41:19 +0700

    indexdb

--- app/db.js
+++ app/db.js
@@ -8,12 +8,27 @@
       return;
     }
   
-    var dbPromise = indexedDB.open('test-db2', 1, function(upgradeDb) {
-      console.log('making a new object store');
-      if (upgradeDb.objectStoreNames.contains('firstOS')) {
-        upgradeDb.createObjectStore('firstOS');
-      }
-    });
+    var dbPromise = indexedDB.open('couches-n-things', 3, function(upgradeDb) {
+        switch (upgradeDb.oldVersion) {
+          case 0:
+            // a placeholder case so that the switch block will
+            // execute when the database is first created
+            // (oldVersion is 0)
+          case 1:
+            console.log('Creating the products object store');
+            upgradeDb.createObjectStore('products', {keyPath: 'id'});
+          case 2:
+            console.log('Creating a name index');
+            var store = upgradeDb.transaction.objectStore('products');
+            store.createIndex('name', 'name', {unique: true});
+      
+          // TODO 4.2 - create 'price' and 'description' indexes
+      
+          // TODO 5.1 - create an 'orders' object store
+      
+        }
+      });
+      
   
   })();
       
