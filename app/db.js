809d73fba876da688e332ec4eb62a0672e27ae20
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 21:04:42 +0700

    indexdb

--- app/db.js
+++ app/db.js
@@ -3,7 +3,6 @@
     //check for support
     if (!('indexedDB' in window)) {
       console.log('This browser doesn\'t support IndexedDB');
-      return;
     }
   
     var dbPromise = indexedDB.open('test-db4', 1, function(db) {
