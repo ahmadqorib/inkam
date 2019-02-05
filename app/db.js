7779e9104b8ba5f1c8b70591f4832aefc0076d87
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 19:28:26 +0700

    indexdb

--- app/db.js
+++ app/db.js
@@ -7,36 +7,10 @@
       return;
     }
   
-    var dbPromise = indexedDB.open('test-db4', 1, function(upgradeDb) {
-      if (!upgradeDb.objectStoreNames.contains('people')) {
-        var peopleOS = upgradeDb.createObjectStore('people', {keyPath: 'email'});
-        peopleOS.createIndex('gender', 'gender', {unique: false});
-        peopleOS.createIndex('ssn', 'ssn', {unique: true});
-      }
-      if (!upgradeDb.objectStoreNames.contains('notes')) {
-        var notesOS = upgradeDb.createObjectStore('notes', {autoIncrement: true});
-        notesOS.createIndex('title', 'title', {unique: false});
-      }
-      if (!upgradeDb.objectStoreNames.contains('logs')) {
-        var logsOS = upgradeDb.createObjectStore('logs', {keyPath: 'id',
-          autoIncrement: true});
-      }
+    var dbPromise = indexedDB.open('test-db4', 1, function(db) {
+        db.createObjectStore('tbl_info');
     });
 
-    dbPromise.then(function(db) {
-        var tx = db.transaction('store', 'readwrite');
-        var store = tx.objectStore('store');
-        var item = {
-          name: 'sandwich',
-          price: 4.99,
-          description: 'A very tasty sandwich',
-          created: new Date().getTime()
-        };
-        store.add(item);
-        return tx.complete;
-      }).then(function() {
-        console.log('added item to the store os!');
-      });
   })();
 
   
