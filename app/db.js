1d45415517ec9aec8aa245250f7cd43692ff79ef
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 21:23:50 +0700

    indexdb

--- app/db.js
+++ app/db.js
@@ -7,20 +7,10 @@
   
     const tabelku = indexedDB.open('dbku', 1, db => {
         // db.createObjectStore('tabelku');
-        db.then(function(db) {
-            var tx = db.transaction('store', 'readwrite');
-            var store = tx.objectStore('store');
-            var item = {
-              name: 'sandwich',
-              price: 4.99,
-              description: 'A very tasty sandwich',
-              created: new Date().getTime()
-            };
-            store.add(item);
-            return tx.complete;
-          }).then(function() {
-            console.log('added item to the store os!');
-          });
+        console.log('making a new object store');
+        if (!db.objectStoreNames.contains('firstOS')) {
+        db.createObjectStore('firstOS');
+        }
       });
 
 
