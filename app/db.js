642a796e56c0b5e4e5de65afd581ea38cdd633d0
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 16:58:24 +0700

    coba

--- app/db.js
+++ app/db.js
@@ -1,7 +1,13 @@
-if (!('indexedDB' in window)) {
-    console.log('This browser doesn\'t support IndexedDB');
-    
-  }else{
-    console.log('This browser support IndexedDB');
-    
-  }
+(function() {
+    'use strict';
+  
+    //check for support
+    if (!('indexedDB' in window)) {
+      console.log('This browser doesn't support IndexedDB');
+      return;
+    }
+  
+    var dbPromise = idb.open('db_inkam', 1);
+  
+  })();
+  
