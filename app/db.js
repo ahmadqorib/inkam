dbebedc2788408275c30e24d02b85bac8811236a
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 16:56:31 +0700

    coba

--- app/db.js
+++ app/db.js
@@ -1,7 +1,7 @@
 if (!('indexedDB' in window)) {
     console.log('This browser doesn\'t support IndexedDB');
-    return;
+    
   }else{
     console.log('This browser support IndexedDB');
-    return;
+    
   }
