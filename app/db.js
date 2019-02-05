afc4777ccff64d2fac58882f25b8df6c11d030e4
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 18:14:53 +0700

    indexdb

--- app/db.js
+++ app/db.js
@@ -1,7 +1,20 @@
 
-const info = indexedDB.open('db_inkam', 1, db => {
+var info = indexedDB.open('db_inkam', 1, db => {
     db.createObjectStore('info');
   });
   
-
+info.then(function(db) {
+    var tx = db.transaction('store', 'readwrite');
+    var store = tx.objectStore('store');
+    var item = {
+      name: 'sandwich',
+      price: 4.99,
+      description: 'A very tasty sandwich',
+      created: new Date().getTime()
+    };
+    store.add(item);
+    return tx.complete;
+  }).then(function() {
+    console.log('added item to the store os!');
+  });
   
