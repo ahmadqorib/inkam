61b581ed87151215c773dbaa24fcc2c852a21233
ahmadqorib <ahmadqorib97@gmail.com>
2019-02-05 21:30:45 +0700

    indexdb

--- app/db.js
+++ app/db.js
@@ -1,19 +1,20 @@
 
   (function() {
     'use strict';
+  
     //check for support
     if (!('indexedDB' in window)) {
       console.log('This browser doesn\'t support IndexedDB');
+      return;
     }
   
-    const tabelku = indexedDB.open('dbku', 1, db => {
-        // db.createObjectStore('tabelku');
-        console.log('making a new object store');
-        if (!db.objectStoreNames.contains('firstOS')) {
-        db.createObjectStore('firstOS');
-        }
-      });
-
-    })();
+    var dbPromise = idb.open('test-db2', 1, function(upgradeDb) {
+      console.log('making a new object store');
+      if (!upgradeDb.objectStoreNames.contains('firstOS')) {
+        upgradeDb.createObjectStore('firstOS');
+      }
+    });
+  
+  })();
       
   
